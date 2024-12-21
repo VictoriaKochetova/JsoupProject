edaac8d750e5573acdf81827745959537b7126e1
Victoria Kochetova <vikah8u@gmail.com>
2024-12-21 21:34:48 +0300

    KS-5-8 parser of quotes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,16 @@
 package org.example
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+
+fun main() {
+    val document: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
+
+    val quotes: Elements = document.select(".sc-2aegk7-2")
+
+    for (quote in quotes) {
+        println("${quote.text()}\n\t")
+    }
+}
