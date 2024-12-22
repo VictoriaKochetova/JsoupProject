2f13a4fc7f624f68a2511fd8e44ebe05575b08e7
Victoria Kochetova <vikah8u@gmail.com>
2024-12-23 00:46:29 +0300

    KS-5-8 parser of quotes. fixed tag

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ import org.jsoup.select.Elements
 fun main() {
     val document: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
 
-    val quotes: Elements = document.select(".sc-2aegk7-2")
+    val quotes: Elements = document.select("article")
 
     for (quote in quotes) {
         println("${quote.text()}\n\t")
